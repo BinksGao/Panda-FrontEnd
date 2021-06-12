38a235dd041b94fa9db84b4228b95c0d0617f437
gaohuan <gaohuan@xs901.com>
2021-06-12 18:19:22 +0800

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,36 @@
+import { MenuEntry } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: t('Trade'),
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: t('Exchange'),
+        href: 'http://localhost:3001/#/swap',
+      },
+      {
+        label: t('Liquidity'),
+        href: 'http://localhost:3001/#/pool',
+      }
+    ],
+  },
+  {
+    label: t('Farms'),
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: t('Pools'),
+    icon: 'PoolIcon',
+    href: '/pools',
+  }
+]
+
+export default config
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Heading, Text, BaseLayout } from '@pancakeswap/uikit'
+import { useTranslation } from 'contexts/Localization'
+import Page from 'components/layout/Page'
+import FarmStakingCard from 'views/Home/components/FarmStakingCard'
+import CakeStats from 'views/Home/components/CakeStats'
+import TotalValueLockedCard from 'views/Home/components/TotalValueLockedCard'
+
+const Hero = styled.div`
+  align-items: center;
+  background-image: url('/images/pan-bg-mobile.svg');
+  background-repeat: no-repeat;
+  background-position: top center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  margin-bottom: 32px;
+  padding-top: 116px;
+  text-align: center;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/pan-bg2.svg'), url('/images/pan-bg.svg');
+    background-position: left center, right center;
+    height: 165px;
+    padding-top: 0;
+  }
+`
+
+const Cards = styled(BaseLayout)`
+  align-items: stretch;
+  justify-content: stretch;
+  margin-bottom: 24px;
+  grid-gap: 24px;
+
+  & > div {
+    grid-column: span 6;
+    width: 100%;
+  }
+`
+
+const CTACards = styled(BaseLayout)`
+  flex-direction: column;
+  & > div {
+    grid-column: span 12;
+    width: 100%;
+    & > div {
+      width: 100%;
+    }
+  }
+`
+
+const Home: React.FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Page>
+      <Hero>
+        <Heading as="h1" scale="xl" mb="24px" color="secondary">
+          {t('PancakeSwap')}
+        </Heading>
+        <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+      </Hero>
+      <div>
+        <Cards>
+          <FarmStakingCard />
+          <CTACards>
+            <CakeStats />
+            <TotalValueLockedCard />
+          </CTACards>
+        </Cards>
+      </div>
+    </Page>
+  )
+}
+
+export default Home
